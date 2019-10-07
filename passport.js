128533b559a6eda00895749fe8699c21363d7641
Gaurav Cherwal <gauravcherwal@outlook.com>
2019-10-07 16:00:28 +0530

    Added Favicon in Birds Website

--- passport.js
+++ passport.js
@@ -1,124 +1,133 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const GithubStrategy = require('passport-github').Strategy
-const FacebookStrategy = require('passport-facebook').Strategy
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const GithubStrategy = require("passport-github").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//const OutlookStrategy = require("passport-outlook").Strategy;
 
-const {
-    Users
-} = require('./db')
+const { Users } = require("./db");
 
-passport.use('local',
-    new LocalStrategy((username, password, done) => {
-        Users.findOne({
-            where: {
-                username,
-            },
-        })
-            .then((user) => {
-                if (!user) {
-                    return done(null, false, {
-                        message: 'Username is not valid'
-                    })
-                }
+passport.use(
+  "local",
+  new LocalStrategy((username, password, done) => {
+    Users.findOne({
+      where: {
+        username
+      }
+    })
+      .then(user => {
+        if (!user) {
+          return done(null, false, {
+            message: "Username is not valid"
+          });
+        }
 
-                if (user.password != password) {
-                    return done(null, false, {
-                        message: 'Password is INCORRECT!!!'
-                    })
-                }
-                done(null, user)
-            })
-            .catch(done)
-    }),
-)
+        if (user.password != password) {
+          return done(null, false, {
+            message: "Password is INCORRECT!!!"
+          });
+        }
+        done(null, user);
+      })
+      .catch(done);
+  })
+);
 
-passport.use(new GithubStrategy({
-    clientID: 'Iv1.a7dc6f7f95100c4d',
-    clientSecret: '1a1dec9a0d5c63327eb6ebfc0be24372ae2932a2',
-    callbackURL: 'https://gamify-v100.herokuapp.com/login/github/callback',
-},
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: "Iv1.a7dc6f7f95100c4d",
+      clientSecret: "1a1dec9a0d5c63327eb6ebfc0be24372ae2932a2",
+      callbackURL: "https://gamify-v100.herokuapp.com/login/github/callback"
+    },
     (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-        Users.findCreateFind({
-            where: {
-                username: profile.id
-            },
-            defaults: {
-                username: profile.id,
-                ghAccessToken: accessToken,
-            },
-        }).then((user) => {
-            //console.log('after this')
-            //console.log(profile.displayName)
-            done(null, profile)
+      console.log(profile);
+      Users.findCreateFind({
+        where: {
+          username: profile.id
+        },
+        defaults: {
+          username: profile.id,
+          ghAccessToken: accessToken
+        }
+      })
+        .then(user => {
+          //console.log('after this')
+          //console.log(profile.displayName)
+          done(null, profile);
         })
-            .catch(done)
-        /* console.log(profile)
+        .catch(done);
+      /* console.log(profile)
         Users.create({
                 username: profile.id,
                 ghAccessToken: accessToken,
             }) */
+    }
+  )
+);
 
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "894130110965374",
+      clientSecret: "2a826c57a5b48b104af9eeb00a60667f",
+      //callbackURL: "http://localhost:7890/login/facebook/callback"
+      callbackURL: "https://gamify-v100.herokuapp.com/login/facebook/callback"
     },
-))
-
-passport.use(new FacebookStrategy({
-    clientID: '894130110965374',
-    clientSecret: '2a826c57a5b48b104af9eeb00a60667f',
-    callbackURL: "https://gamify-v100.herokuapp.com/login/facebook/callback"
-},
     (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-        Users.findCreateFind({
-            where: {
-                username: profile.id
-            },
-            defaults: {
-                username: profile.id,
-                fbAcessToken: accessToken,
-            }
-        }).then((user) => {
-            console.log('successful authentication via fb')
-            done(null, profile)
+      console.log(profile);
+      Users.findCreateFind({
+        where: {
+          username: profile.id
+        },
+        defaults: {
+          username: profile.id,
+          fbAcessToken: accessToken
+        }
+      })
+        .then(user => {
+          console.log("successful authentication via fb");
+          done(null, profile);
         })
-            .catch(done)
+        .catch(done);
     }
-));
+  )
+);
 
-
-passport.use(new GoogleStrategy({
-    clientID: '960091983754-jdg7m286hslrek15bqio3cd0dg6osf47.apps.googleusercontent.com',
-    clientSecret: 'lcahPdCCbzTOJuRlFSCGGIFn',
-    callbackURL: "https://gamify-v100.herokuapp.com/login/google/callback"
-},
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "960091983754-jdg7m286hslrek15bqio3cd0dg6osf47.apps.googleusercontent.com",
+      clientSecret: "lcahPdCCbzTOJuRlFSCGGIFn",
+      callbackURL: "https://gamify-v100.herokuapp.com/login/google/callback"
+    },
     (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-        Users.findCreateFind({
-            where: {
-                username: profile.id
-            },
-            defaults: {
-                username: profile.id,
-                gleAcessToken: accessToken,
-            }
-        }).then((user) => {
-            console.log('successful authentication via google')
-            done(null, profile)
+      console.log(profile);
+      Users.findCreateFind({
+        where: {
+          username: profile.id
+        },
+        defaults: {
+          username: profile.id,
+          gleAcessToken: accessToken
+        }
+      })
+        .then(user => {
+          console.log("successful authentication via google");
+          done(null, profile);
         })
-            .catch(done)
+        .catch(done);
     }
-));
+  )
+);
 
-
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-
-    done(null, user);
+passport.deserializeUser(function(user, done) {
+  done(null, user);
 });
 /* passport.serializeUser(function (user, done) {
     done(null, user.id);
@@ -134,4 +143,4 @@ passport.deserializeUser((userId, done) => {
         .catch(done)
 }) */
 
-module.exports = passport
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,339 +1,333 @@
-const express = require('express')
-const session = require('express-session')
-const passport = require('./passport')
-const flash = require('express-flash')
-const cookieParser = require('cookie-parser');
-const Auth = require('connect-ensure-login')
-const path = require('path')
-const SQLiteStore = require('connect-sqlite3')(session);
-const port = process.env.PORT || 7890;
+const express = require("express");
+const session = require("express-session");
+const passport = require("./passport");
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+const Auth = require("connect-ensure-login");
+const path = require("path");
+const SQLiteStore = require("connect-sqlite3")(session);
+const port = 7890;
 
+let { db, Users, Games, Sequelize } = require("./db");
 
+let userone = {};
 
-let {
-    db,
-    Users,
-    Games,
-    Sequelize
-} = require('./db')
+let flag = 1;
 
-let userone = {}
+const app = express();
 
-let flag = 1
+app.set("view engine", "hbs");
 
-const app = express()
+app.use(express.json());
 
-app.set('view engine', 'hbs')
-
-app.use(express.json())
-
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}))
+  })
+);
 
-app.use(cookieParser('secret'));
+app.use(cookieParser("secret"));
 
-app.use(session({
-    store: new SQLiteStore,
-    secret: 'averylongstring',
+app.use(
+  session({
+    store: new SQLiteStore(),
+    secret: "averylongstring",
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 60 * 60 * 1000,
-    },
-}))
+      maxAge: 60 * 60 * 1000
+    }
+  })
+);
 
-app.use(flash())
+app.use(flash());
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-app.use(passport.session())
+app.use(passport.session());
 
 app.use((req, res, next) => {
-    res.locals.login = req.isAuthenticated();
-    next();
-})
-
-app.get('/', (req, res) => {
-
-    Games.findAll({
-        order: [
-            ['Likes', 'DESC']
-        ],
-    }).then((game) => {
-
-        if (req.user) {
-            console.log('inside if')
-            const all = game
-            if (req.user.username != undefined) {
-
-                res.render('index', {
-                    user: req.user.username, all
-                })
-            } else {
-                res.render('index', {
-                    user: req.user.displayName, all
-                })
-            }
-        } else {
-            const all = game
-            res.render('index', { all })
-        }
-    }).catch((err) => console.log(err))
-})
-
-
-app.get('/signup', (req, res) => {
-    res.render('signup')
-})
-
-app.get('/login', (req, res) => {
-    res.render('login')
-    flag = 1
-})
-
-const isAdmin = (req, res, next, ) => {
-
-    if (req.user.username == 'test') {
-        userone.Name = 'test'
-        return next()
-    } else {
-        req.flash('info', 'Access Denied!!!')
-        res.redirect('/')
-        return
-
-    }
-}
-
-app.get('/admin', Auth.ensureLoggedIn('/login'), isAdmin, (req, res) => {
-    res.render('admin', {
-        user: req.user
-    })
-})
-
-app.get('/show', Auth.ensureLoggedIn('/login'), (req, res) => {
-
-    Games.findAll().then((el) => {
-        const ga = el
-        res.render('show', {
-            ga
-        })
-    })
-})
-
-
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-
-
-app.get('/game', (req, res) => {
-    let ans = req.query.game
-    if (ans == undefined) {
-        req.flash('error', 'Please Enter Title of Game!!!')
-        res.redirect('/')
-        return
-    }
-    res.redirect('/game/' + ans)
-})
-
-app.get('/game/:Name', (req, res) => {
-
-    const ans = req.params.Name.toLowerCase()
-    Games.findOne({
-        where: {
-            Name: Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('Name')), 'LIKE', '%' + ans + '%')
-        },
-    }).then((game) => {
-        if (!game) {
-            req.flash('error', 'Sorry, Game not found!!!')
-            res.redirect('/')
-            return
-        }
-        res.render('game', { game, flag })
-    }).catch((err) => console.log('error'))
-})
-
-
-app.get('/logout', function (req, res) {
-    req.session.destroy(function (err) {
-        res.redirect('/');
-    });
+  res.locals.login = req.isAuthenticated();
+  next();
 });
 
-
-app.get('/login/github', passport.authenticate('github'))
-
-app.get('/login/github/callback', passport.authenticate('github', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-}))
-
-app.get('/login/facebook', passport.authenticate('facebook'))
-
-app.get('/login/facebook/callback', passport.authenticate('facebook', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-app.get('/login/google', passport.authenticate('google', { scope: ['profile'] }));
-
-
-app.get('/login/google/callback', passport.authenticate('google', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-
-
-
-
-app.get('/support', (req, res) => res.render('support'))
-
-app.get('/admin/del', Auth.ensureLoggedIn('/login'), isAdmin, (req, res) => {
-    res.render('admindelete', {
-        user: req.user
+app.get("/", (req, res) => {
+  Games.findAll({
+    order: [["Likes", "DESC"]]
+  })
+    .then(game => {
+      if (req.user) {
+        console.log("inside if");
+        const all = game;
+        if (req.user.username != undefined) {
+          res.render("index", {
+            user: req.user.username,
+            all
+          });
+        } else {
+          res.render("index", {
+            user: req.user.displayName,
+            all
+          });
+        }
+      } else {
+        const all = game;
+        res.render("index", { all });
+      }
     })
-})
+    .catch(err => console.log(err));
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+  flag = 1;
+});
+
+const isAdmin = (req, res, next) => {
+  if (req.user.username == "test") {
+    userone.Name = "test";
+    return next();
+  } else {
+    req.flash("info", "Access Denied!!!");
+    res.redirect("/");
+    return;
+  }
+};
+
+app.get("/admin", Auth.ensureLoggedIn("/login"), isAdmin, (req, res) => {
+  res.render("admin", {
+    user: req.user
+  });
+});
+
+app.get("/show", Auth.ensureLoggedIn("/login"), (req, res) => {
+  Games.findAll().then(el => {
+    const ga = el;
+    res.render("show", {
+      ga
+    });
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/game", (req, res) => {
+  let ans = req.query.game;
+  if (ans == undefined) {
+    req.flash("error", "Please Enter Title of Game!!!");
+    res.redirect("/");
+    return;
+  }
+  res.redirect("/game/" + ans);
+});
+
+app.get("/game/:Name", (req, res) => {
+  const ans = req.params.Name.toLowerCase();
+  Games.findOne({
+    where: {
+      Name: Sequelize.where(
+        Sequelize.fn("LOWER", Sequelize.col("Name")),
+        "LIKE",
+        "%" + ans + "%"
+      )
+    }
+  })
+    .then(game => {
+      if (!game) {
+        req.flash("error", "Sorry, Game not found!!!");
+        res.redirect("/");
+        return;
+      }
+      res.render("game", { game, flag });
+    })
+    .catch(err => console.log("error"));
+});
+
+app.get("/logout", function(req, res) {
+  req.session.destroy(function(err) {
+    res.redirect("/");
+  });
+});
+
+app.get("/login/github", passport.authenticate("github"));
+
+app.get(
+  "/login/github/callback",
+  passport.authenticate("github", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
+
+app.get("/login/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/login/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+app.get(
+  "/login/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/login/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+app.get("/support", (req, res) => res.render("support"));
+
+app.get("/admin/del", Auth.ensureLoggedIn("/login"), isAdmin, (req, res) => {
+  res.render("admindelete", {
+    user: req.user
+  });
+});
 /////* GET Ends Here */////
 
-
-
-app.post('/login', passport.authenticate('local', {
-    successReturnToOrRedirect: '/',
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successReturnToOrRedirect: "/",
     failureRedirect: "/login",
-    failureFlash: true,
-}))
+    failureFlash: true
+  })
+);
 
-app.post('/signup', (req, res) => {
-    const newuser = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+app.post("/signup", (req, res) => {
+  const newuser = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  };
+  if (newuser.username == "") {
+    req.flash("error", "Please Enter Username!!");
+    res.redirect("/signup");
+    return;
+  }
+  if (newuser.email == "") {
+    req.flash("error", "Please Enter Valid Email!!");
+    res.redirect("/signup");
+    return;
+  }
+  if (newuser.password == "") {
+    req.flash("error", "Please Enter Password!!");
+    res.redirect("/signup");
+    return;
+  }
+  Users.create(newuser)
+    .then(el => {
+      res.redirect("/login");
+    })
+    .catch(err => console.log(err));
+});
+
+app.post("/game/:Name", Auth.ensureLoggedIn("/login"), (req, res) => {
+  const ans = req.params.Name;
+  if (flag == 1) {
+    flag = 0;
+    Games.findOne({
+      where: {
+        Name: ans
+      }
+    }).then(game => {
+      let inc = game.Likes;
+      inc++;
+      Games.update({ Likes: inc }, { where: { Name: game.Name } })
+        .then(
+          result => res.redirect("/game/" + game.Name),
+
+          console.log("done successfully!!")
+        )
+        .catch(err => console.log(err));
+    });
+  } else {
+    flag = 1;
+    console.log(ans.id);
+    Games.findOne({
+      where: {
+        Name: ans
+      }
+    }).then(game => {
+      let dec = game.Likes;
+      dec--;
+      Games.update({ Likes: dec }, { where: { Name: game.Name } })
+        .then(result => res.redirect("/game/" + game.Name))
+        .catch(err => console.log(err));
+    });
+  }
+});
+
+app.post("/admin", (req, res, next) => {
+  const newgame = {
+    id: req.body.id,
+    Name: req.body.title,
+    Genre: req.body.genre,
+    Image: req.body.image,
+    URL: req.body.url
+  };
+
+  if (
+    newgame.id == "" ||
+    newgame.Name == "" ||
+    newgame.Genre == "" ||
+    newgame.Image == "" ||
+    newgame.URL == ""
+  ) {
+    req.flash("error", "Please fill all the Details!!!");
+    res.redirect("/admin");
+    return;
+  }
+  Games.create(newgame)
+    .then(el => {
+      req.flash("successMessage", "Game was added successfully!!!");
+      res.redirect("/admin");
+    })
+    .catch(err => console.log(err));
+});
+
+app.post("/admin/del", (req, res) => {
+  Games.destroy({
+    where: {
+      id: req.body.ID
     }
-    if (newuser.username == '') {
-        req.flash('error', 'Please Enter Username!!')
-        res.redirect('/signup')
-        return
-    }
-    if (newuser.email == '') {
-        req.flash('error', 'Please Enter Valid Email!!')
-        res.redirect('/signup')
-        return
-    }
-    if (newuser.password == '') {
-        req.flash('error', 'Please Enter Password!!')
-        res.redirect('/signup')
-        return
-    }
-    Users.create(newuser).then((el) => {
-        res.redirect('/login')
-    }).catch((err) => console.log(err))
-})
-
-
-app.post('/game/:Name', Auth.ensureLoggedIn('/login'), (req, res) => {
-
-    const ans = req.params.Name
-    if (flag == 1) {
-        flag = 0
-        Games.findOne({
-            where: {
-                Name: ans
-            }
-        }).then((game) => {
-            let inc = game.Likes
-            inc++
-            Games.update(
-                { Likes: inc },
-                { where: { Name: game.Name } }
-            )
-                .then((result) =>
-                    res.redirect('/game/' + game.Name),
-
-                    console.log('done successfully!!')
-                )
-                .catch(err =>
-                    console.log(err)
-                )
-        })
-    } else {
-        flag = 1
-        console.log(ans.id)
-        Games.findOne({
-            where: {
-                Name: ans
-            }
-        }).then((game) => {
-            let dec = game.Likes
-            dec--
-            Games.update(
-                { Likes: dec },
-                { where: { Name: game.Name } }
-            ).then((result) =>
-                res.redirect('/game/' + game.Name),
-
-            )
-                .catch((err) =>
-                    console.log(err)
-                )
-        })
-
-    }
-})
-
-
-
-app.post('/admin', (req, res, next) => {
-    const newgame = {
-        id: req.body.id,
-        Name: req.body.title,
-        Genre: req.body.genre,
-        Image: req.body.image,
-        URL: req.body.url,
-    }
-
-
-    if (newgame.id == '' || newgame.Name == '' || newgame.Genre == '' || newgame.Image == '' || newgame.URL == '') {
-        req.flash('error', 'Please fill all the Details!!!')
-        res.redirect('/admin')
-        return
-    }
-    Games.create(newgame).then((el) => {
-        req.flash('successMessage', 'Game was added successfully!!!')
-        res.redirect('/admin')
-    }).catch((err) => console.log(err))
-
-})
-
-app.post('/admin/del', (req, res) => {
-    Games.destroy({
-        where: {
-            id: req.body.ID,
-        },
-    }).then((game) => {
-        if (!game) {
-            req.flash('error', 'Game not Found!!!')
-            res.redirect('/admin/del')
-            return
-        }
-        console.log('deleted')
-        res.redirect('/')
-    }).catch((err) => console.log(err))
-})
+  })
+    .then(game => {
+      if (!game) {
+        req.flash("error", "Game not Found!!!");
+        res.redirect("/admin/del");
+        return;
+      }
+      console.log("deleted");
+      res.redirect("/");
+    })
+    .catch(err => console.log(err));
+});
 
 /// POST Ends Here!!!!
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(express.static(path.join(__dirname, '/public/images')))
+app.use(express.static(path.join(__dirname, "/public/images")));
 
-app.use('/admin', express.static(__dirname + '/public'))
+app.use("/admin", express.static(__dirname + "/public"));
 
 db.sync().then(() => {
-    app.listen(port, () => {
-        console.log('listening to port')
-    })
-})
+  app.listen(port, () => {
+    console.log("listening to port");
+  });
+});
